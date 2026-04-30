dce043a29204433dd8193bc409462ac6e6e2788d
Prots Khrystyna <105975981+khrustja@users.noreply.github.com>
2026-04-30 09:13:45 +0300

    Delete SchoolGradesMvcSite directory

--- SchoolGradesMvcSite/ViewModels/ClassStatisticsViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SchoolGradesMvcSite.ViewModels;
-
-public class ClassStatisticsViewModel
-{
-    public string ClassName { get; set; } = string.Empty;
-    public int StudentsCount { get; set; }
-    public decimal Average { get; set; }
-    public int GradesCount { get; set; }
-}
